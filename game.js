b2aad82ae45aefa3583b41f2e6a85bc8fa8a8a56
Scott Barclay <93163082+sbrcly@users.noreply.github.com>
2021-10-26 20:49:50 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 class Game {
-    constructor(letterBtns, categoryOptions, mysteryValueHolder, livesLeft) {
+    constructor(gameContainer, letterBtns, difficultyBtn, categoryOptions, mysteryValueHolder, livesLeft) {
+        this.gameContainer = gameContainer;
         this.letterBtns = letterBtns;
+        this.difficultyBtn = difficultyBtn;
         this.categoryOptions = categoryOptions;
         this.mysteryValueHolder = mysteryValueHolder;
         this.livesLeft = livesLeft;
@@ -9,18 +11,28 @@ class Game {
         this.hideTerm = undefined;
 
         this.shownLetters = [];
-        livesLeft.innerText = 7;
+        livesLeft.innerText = 9;
 
         for (let letter of this.letterBtns) {
             letter.addEventListener('click', () => {
-                this.pickLetter(letter);
-            }) 
+                this.pickLetterClick(letter);
+            });
         };
         categoryOptions.addEventListener('change', this.chooseMysteryValue);
+        difficultyBtn.addEventListener('change', this.changeDifficulty);
+        window.addEventListener('keypress', (e) => {
+            for (let letter of this.letterBtns) {
+                if (letter.id === e.key) {
+                    if (letter.classList.contains('wrong') === false) {
+                        this.pickLetterType(e);
+                    }
+                }
+            }
+        })
 
         this.chooseMysteryValue();
-    }
-    pickLetter = (letter) => {
+    };
+    pickLetterClick = (letter) => {
         letter.disabled = true;
         if (this.mysteryTerm.toLowerCase().indexOf(letter.id) != -1) {
             letter.classList.add('correct');
@@ -28,9 +40,36 @@ class Game {
         }   else {
             letter.classList.add('wrong');
             this.livesLeft.innerText = this.livesLeft.innerText - 1;
-            if (this.livesLeft.innerText < 6) this.livesLeft.style.backgroundColor = 'rgb(104, 41, 41)';
+            if (this.livesLeft.innerText < 4) this.livesLeft.style.backgroundColor = 'rgb(104, 41, 41)';
             if (this.livesLeft.innerText == 0) this.gameOver();
-        }
+        };
+    };
+    pickLetterType = (e) => {
+        if (this.mysteryTerm.toLowerCase().indexOf(e.key) != -1) {
+            for (let letter of this.letterBtns) {
+                if (letter.id === e.key) {
+                    letter.classList.add('correct');
+                    this.displayLetter(letter);
+                };
+            };
+        }   else {
+            for (let letter of this.letterBtns) {
+                if (letter.id === e.key) {
+                    letter.disabled = true;
+                    letter.classList.add('wrong');
+                    this.livesLeft.innerText = this.livesLeft.innerText - 1;
+                    if (this.livesLeft.innerText < 4) this.livesLeft.style.backgroundColor = 'rgb(104, 41, 41)';
+                    if (this.livesLeft.innerText == 0) this.gameOver();
+                };
+            };
+        };
+    };
+    changeDifficulty = () => {
+        if (this.difficultyBtn.value === 'easy') this.livesLeft.innerText = 9; 
+        if (this.difficultyBtn.value === 'medium') this.livesLeft.innerText = 6;
+        if (this.difficultyBtn.value === 'hard') this.livesLeft.innerText = 4
+        this.resetGame();
+        this.displayMysteryTerm();
     }
     chooseMysteryValue = () => {
         this.resetGame();
@@ -39,15 +78,15 @@ class Game {
         const random = Math.floor(Math.random() * mysteryTerms[getCatIndex].length);
         this.mysteryTerm = mysteryTerms[getCatIndex][random];
         this.displayMysteryTerm();
-    }
+    };
     displayMysteryTerm = () => {
         let placeholder = this.mysteryTerm;
         for (let char of alphabet) {
            this.hideTerm = placeholder.toLowerCase().replaceAll(`${char}`, '_');
            placeholder = this.hideTerm;
            this.mysteryValueHolder.innerText = this.hideTerm;
-        }
-    }
+        };
+    };
     displayLetter = (letter) => {
         this.shownLetters.push(letter.id);
         let showLetter;
@@ -56,52 +95,54 @@ class Game {
             for (let c of this.shownLetters) {
                 if (this.mysteryTerm[i].toLowerCase() != c) {
                     placeholder[i] = '_';
-                }
-            }
-        }
+                };
+            };
+        };
         for (let j = 0; j < this.mysteryTerm.length; j++) {
             for (let c of this.shownLetters) {
                 if (this.mysteryTerm[j].toLowerCase() === c) {
                     placeholder[j] = c;
-                }
-            }
-        }
+                };
+            };
+        };
         for (let k = 0; k < this.mysteryTerm.length; k++) {
             for (let c of this.shownLetters) {
                 if (this.mysteryTerm[k].toLowerCase() === ' ') {
                     placeholder[k] = '  ';
-                }
-            }
-        }
+                };
+            };
+        };
         this.mysteryValueHolder.innerText = placeholder.join('');
         if (this.mysteryValueHolder.innerText.indexOf('_') === -1) this.chickenDinner();
-    }
+    };
     chickenDinner = () => {
         for (let letter of this.letterBtns) {
             letter.disabled = true;
-        }
-        setTimeout(() => {
-            alert('You Win!')
-        }, 500);
-        this.livesLeft.style.backgroundColor = 'rgb(41, 104, 41)'
-    }
+        };
+        this.gameContainer.classList.remove('lose');
+        this.gameContainer.classList.add('win');
+        this.livesLeft.style.backgroundColor = 'rgb(41, 104, 41)';
+        this.difficultyBtn.style.opacity = 0;
+    };
     gameOver = () => {
         for (let letter of this.letterBtns) {
             letter.disabled = true;
-        }
-        setTimeout(() => {
-            alert('You lose!')
-        }, 500);
+        };
+        this.gameContainer.classList.remove('win');
+        this.gameContainer.classList.add('lose');
         this.mysteryValueHolder.innerText = this.mysteryTerm;
-    }
+        this.difficultyBtn.style.opacity = 0;
+    };
     resetGame = () => {
         this.shownLetters = [];
-        this.livesLeft.innerText = 7;
         for (let letter of this.letterBtns) {
             letter.disabled = false;
             letter.classList.remove('correct');
             letter.classList.remove('wrong');
-        }
+        };
+        if (this.difficultyBtn.value === 'easy') this.livesLeft.innerText = 9; 
+        if (this.difficultyBtn.value === 'medium') this.livesLeft.innerText = 6;
+        if (this.difficultyBtn.value === 'hard') this.livesLeft.innerText = 4
         this.livesLeft.style.backgroundColor = 'rgb(41, 104, 41)';
-    }
+    };
 };
